3d3a89c163ce9f4c45a7783e60a16b20e3e7a9ed
Simphiwe Nkwanyana <sbnkwanyana@hotmail.com>
2023-05-04 11:00:49 +0200

    url fixes

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,14 +1,14 @@
 #!/usr/bin/node
 const request = require('request');
-const URL = 'https://swapi-api.alx-tools.com/api/?format=json';
+const URL = 'https://swapi-api.alx-tools.com/api';
 
 if (process.argv.length > 2) {
-  request(`${URL}/films/${process.argv[2]}/`, (err, _, body) => {
+  request(`${URL}/films/${process.argv[2]}/?format=json`, (err, _, body) => {
     if (err) {
       console.log(err);
     }
-    const charactersURL = JSON.parse(body).characters;
-    const charactersName = charactersURL.map(
+    const characterURLs = JSON.parse(body).characters;
+    const charactersName = characterURLs.map(
       url => new Promise((resolve, reject) => {
         request(url, (promiseErr, __, charactersReqBody) => {
           if (promiseErr) {
